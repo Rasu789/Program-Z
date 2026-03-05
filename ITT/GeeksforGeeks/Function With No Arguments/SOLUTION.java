5f0047cbb3a1a2a45ce0d931f4f0216fd25d89b5
ARJUN3691 <arjunoct005@gmail.com>
2026-03-05 21:37:11 +0530

    Create SOLUTION.java

---
+++ ITT/GeeksforGeeks/Function With No Arguments/SOLUTION.java
@@ -0,0 +1,11 @@
+class Solution {
+    //  code here
+    public static void main(String [] args)
+    {
+        helloFunction();
+    }
+    static void helloFunction()
+    {
+        System.out.println("Hello");
+    }
+}
