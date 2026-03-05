cdc99ee10c8aaf0438682f812d06b97477c3aa55
ARJUN3691 <arjunoct005@gmail.com>
2026-03-05 21:42:23 +0530

    Create SOLUTION.java

---
+++ ITT/GeeksforGeeks/Java Input Output/SOLUTION.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static void main(String[] args)
+    {
+        String S="GeeksforGeeks";
+        int N=20;
+        printIntString(S,N);
+    }
+    static void printIntString(String S, int N) {
+        // code here
+        System.out.println("The input string :"+S);
+        System.out.println("The input integer :"+N);
+        
+    }
+}
