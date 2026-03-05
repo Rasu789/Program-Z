a5e5da92bd01c84e23511f407b1e7090f370524a
ARJUN3691 <arjunoct005@gmail.com>
2026-03-05 21:32:55 +0530

    Create SOLUTION.java

---
+++ ITT/GeeksforGeeks/The Else Statement/SOLUTION.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>100)
+        {
+            System.out.println("Big");
+        }
+        else if(a<=100)
+        {
+            System.out.println("Small");
+        }
+    }
+}
