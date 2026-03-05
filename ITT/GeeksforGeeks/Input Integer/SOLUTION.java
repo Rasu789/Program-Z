90ca1f126445d2c7649c0aa560d6fc6005c00d18
ARJUN3691 <arjunoct005@gmail.com>
2026-03-05 21:34:07 +0530

    Create SOLUTION.java

---
+++ ITT/GeeksforGeeks/Input Integer/SOLUTION.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=n+10;
+        System.out.println(sum);
+    }
+}
