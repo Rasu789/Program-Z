0b7bbb02b8ec92e924e6d94defa251c26a20234c
ARJUN3691 <arjunoct005@gmail.com>
2026-03-05 21:05:46 +0530

    Create SOLUTION.java

---
+++ ITT/GeeksforGeeks/Java Hello World/SOLUTION.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static void main(String [] args)
+    {
+        printHelloWorld();
+    }
+    static void printHelloWorld() {
+       System.out.println("Hello World");
+        
+    }
+}
