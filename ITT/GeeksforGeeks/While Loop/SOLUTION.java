79b43c113531d839eeb961e09b865db03aa3b8d2
ARJUN3691 <arjunoct005@gmail.com>
2026-03-05 21:45:41 +0530

    Create SOLUTION.java

---
+++ ITT/GeeksforGeeks/While Loop/SOLUTION.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static void main(String [] args)
+    {
+        int x=3;
+        utility(x);
+    }
+    public static void utility(int x) {
+        // code here
+        
+        while(x>=0)
+        {
+            System.out.print(x+" ");
+            x--;
+        }
+        
+    }
+}
