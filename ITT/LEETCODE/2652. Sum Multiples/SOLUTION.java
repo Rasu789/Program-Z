d0b64346cce38f2e4c0fc405c5a453a0d68aa1eb
ARJUN3691 <arjunoct005@gmail.com>
2026-03-15 12:21:36 +0530

    Implement sumOfMultiples method in Solution class

---
+++ ITT/LEETCODE/2652. Sum Multiples/SOLUTION.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOfMultiples(int n) {
+        int sum=0;
+        for(int i=n;i>0;i--)
+        {
+            if(i%7==0 || i%5==0 || i%3==0)
+            {
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+}
