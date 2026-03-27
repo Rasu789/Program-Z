e2d67b22d1ec0ff2fd4e84ae47d80db6b5d9181f
ARJUN E G <arjunoct005@gmail.com>
2026-03-27 10:28:19 +0530

    Add SOLUTION.cpp with main function and calculations

---
+++ ITT/NEOPAT/operator/SOLUTION.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    int u,v;
+    int i,j,k;
+    cin>>a>>b>>c;
+    cin>>u>>v;
+    cin>>i>>j>>k;
+    int x=(((a+b)*5)/4)+((c%3)*5);
+    
+    
+}
