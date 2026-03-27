726fe0469044d4020383d2c188371c04e9523fc8
ARJUN E G <arjunoct005@gmail.com>
2026-03-27 10:39:36 +0530

    Create SOLUTION.java

---
+++ ITT/NEOPAT/REPEATING ELEMENT/SOLUTION.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int min = -1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    min = i;
+                    break;
+                }
+            }
+            if (min != -1) {
+                break;
+            }
+          }
+        if (min != -1)
+            System.out.println("The first repeating element is " + arr[min]);
+        else
+            System.out.println("There are no repeating elements");
+    }
+}
