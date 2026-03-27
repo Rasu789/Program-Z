6dd7276a3b6c87938a569286a79ffc4082b91567
ARJUN E G <arjunoct005@gmail.com>
2026-03-27 10:48:21 +0530

    Create SOLUTION.cpp

---
+++ ITT/NEOPAT/maxandmin/SOLUTION.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    if(n<10)
+    {
+      for(int i=0;i<n;i++)
+      {
+        cin>>a[i];
+      }
+      int aa=a[0];
+      for(int i=1;i<n;i++)
+      {
+        if(a[i]>aa)
+        {
+            aa=a[i];
+        }
+        else if(a[i]<aa)
+        {
+            aa=
+        }
+      }
+   }
+}
