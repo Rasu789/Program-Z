c1af4ab33d8b8e40e888f6611554305082399343
Rasu789 <207437265+Rasu789@users.noreply.github.com>
2026-04-10 12:00:47 +0530

    Delete ITT/LEETCODE directory

--- ITT/LEETCODE/69. Sqrt(x)/SOLUTION.java
+++
@@ -1,6 +0,0 @@
-class Solution {
-    public int mySqrt(int x) {
-            double s=Math.sqrt(x);
-            return (int)s;
-    }
-}
--- ITT/LEETCODE/709. To Lower Case/SOLUTION.java
+++
@@ -1,6 +0,0 @@
-class Solution {
-    public String toLowerCase(String s) {
-        String ss=s.toLowerCase();
-        return ss;
-    }
-}
